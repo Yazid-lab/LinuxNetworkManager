bd864469d0ba8a595e64fc0f9d72891f72f1a650
Bougrine <yazidbougrine99@gmail.com>
2022-07-15 16:26:30 +0100

    changed tool folder and refactored commands

--- proto_tool.sh
+++ proto_tool.sh
@@ -4,23 +4,23 @@ read -r answer
 case "${answer}" in
     1)
         echo "compiling proto files"
-        python -m grpc_tools.protoc --proto_path=. --python_out=.. --grpc_python_out=.. network_manager.proto
+        python -m grpc_tools.protoc --proto_path=protos/ --python_out=. --grpc_python_out=. protos/network_manager.proto
     ;;
     2)
         echo "enter interface name to turn off"
-        read -r -a interfaceName
+        read -r interfaceName
         argumentArray=(--plaintext -proto protos/network_manager.proto -d '{"name" : "'${interfaceName}'"}' localhost:5000 Manager.turn_off_interface)
         grpcurl "${argumentArray[@]}"
     ;;
     3)
         echo "enter interface name to turn on"
-        read -r -a interfaceName
+        read -r interfaceName
         argumentArray=(--plaintext -proto protos/network_manager.proto -d '{"name" : "'${interfaceName}'"}' localhost:5000 Manager.turn_on_interface)
         grpcurl "${argumentArray[@]}"
     ;;
     4)
         echo "enter interface name "
-        read -r -a interfaceName
+        read -r interfaceName
         argumentArray=(--plaintext -proto protos/network_manager.proto -d '{"name" : "'${interfaceName}'"}' localhost:5000 Manager.show_one_interface)
         grpcurl "${argumentArray[@]}"
     ;;
